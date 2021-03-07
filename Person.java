ca575c616518de6cbe665db7a4ef5bc976bf133b
JSylleLoreineTubola <tubola.jsylle@gmail.com>
2021-03-07 13:55:30 +0800

    Get and Set

---
+++ Person.java
@@ -0,0 +1,24 @@
+public class Person {
+		
+	private String firstname, lastname;
+
+    public String getFN()
+	{
+		return firstname;
+	}
+	
+	public String getLN()
+	{
+		return lastname;
+	}
+	
+	public void setFN(String FirstName)
+	{
+		firstname= FirstName;
+	}
+	
+	public void setLN(String LastName)
+	{
+		lastname= LastName;
+	}
+}
